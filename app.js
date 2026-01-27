4bd1736eeb5ec3af02f0f4e250fdaf794ea3a190
AP <apirker@cure53.de>
2026-01-27 09:38:29 +0100

    initial nodejs app

---
+++ app.js
@@ -0,0 +1,29 @@
+const request = require('sync-request')
+const express = require('express')
+const app = express()
+
+let port = 3000
+
+const { exec } = require('node:child_process');
+
+app.use(express.raw({ type: '*/*', limit: '10mb' }));
+
+app.post('/*', (req, res) => {  
+  console.log('New POST (post-redirect) request received') 
+  console.log(req.originalUrl)
+
+  
+  //works!
+  res.redirect(301, `http://localhost/`)
+})
+
+app.get('/*', (req, res) => {  
+  console.log('New request received --> SSRFed') 
+  console.log(req.originalUrl)
+  //works!
+  res.send(200, `you leak the response!!!`)
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
